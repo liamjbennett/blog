01221d2481924a059c70c1f51566e8b4b4c6abdc
Liam Bennett <liamjbennett@gmail.com>
2023-01-27 16:45:53 +0000

    cleaning up theme upgrade

--- refresh-theme.sh
+++ refresh-theme.sh
@@ -2,9 +2,10 @@ if [ -d "themes/hugo-PaperMod" ]
 then
     git submodule deinit -f themes/hugo-PaperMod
     cd themes
-    git rm hugo-PaperMod
+    git rm -f hugo-PaperMod
     rm -rf hugo-PaperMod
     rm -rf .git/modules/themes/hugo-PaperMod
 else
-    git submodule add https://github.com/adityatelange/hugo-PaperMod
+    cd themes
+    git submodule add --force https://github.com/adityatelange/hugo-PaperMod
 fi
