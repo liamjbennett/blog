e2f085de36d9af2d84c45ac09e2ab2366dd8ca66
Liam Bennett <liamjbennett@gmail.com>
2023-01-27 16:39:23 +0000

    refreshing theme

--- refresh-theme.sh
+++ refresh-theme.sh
@@ -1,4 +1,10 @@
-rm -rf themes/hugo-PaperMod
-git submodule deinit -f themes/hugo-PaperMod
-cd themes
-git submodule add https://github.com/adityatelange/hugo-PaperMod
+if [ -d "themes/hugo-PaperMod" ]
+then
+    git submodule deinit -f themes/hugo-PaperMod
+    cd themes
+    git rm hugo-PaperMod
+    rm -rf hugo-PaperMod
+    rm -rf .git/modules/themes/hugo-PaperMod
+else
+    git submodule add https://github.com/adityatelange/hugo-PaperMod
+fi
